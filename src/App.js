ddae1a6fa8824ce507cced12747e2ffe8c50e743
Md Antor <md.antor1155@gmail.com>
2022-05-01 14:15:21 +0800

    added all the pages name, installed react router and make basic routes for home and not-found page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Homepage from './pages/Homepage/Home/Homepage';
+import NotFound from './pages/shared/NotFound/NotFound';
 
 function App() {
   return (
     <div className="App">
-   
+      <Routes>
+        <Route path="/" element={<Homepage></Homepage>}></Route>
+        <Route path="/home" element={<Homepage></Homepage>}></Route>
+        <Route path="*" element={<NotFound></NotFound>}></Route>
+      </Routes>
+
     </div>
   );
 }
---
+++ src/pages/shared/NotFound/NotFound.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NotFound = () => {
+    return (
+        <div>
+            <h1>nothing found here</h1>
+        </div>
+    );
+};
+
+export default NotFound;
