56b1a2bacba3e20d4162114aaf6d653320ada2ec
Md Antor <md.antor1155@gmail.com>
2022-05-05 10:50:07 +0800

    homepage inventory section design done without functional work

--- src/pages/Homepage/Banner/Banner.js
+++ src/pages/Homepage/Banner/Banner.js
@@ -19,14 +19,14 @@ const Banner = () => {
                      <p>If you face any difficulty or need help, happy to be contacted. We have a 24 hour support session.
                      </p>
                      <a target="_blank"  rel="noreferrer" href="https://www.facebook.com/md.antor.16
-                     "><button className='link-button px-4 py-2'> visit support</button></a>
+                     "><button className='customButton px-4 py-2'> visit support</button></a>
                      
                  </div>
                  <div className='testing felxbox'>
                      <h2 className='testing'>Our sourcing sides</h2>
                      <p>We have done business over 20 years and around 50 countries. With good experiences comes good sources.</p>
                      <a target="_blank"  rel="noreferrer" href="https://www.facebook.com/md.antor.16
-                     "><button className='link-button px-4 py-2'> visit sources</button></a>
+                     "><button className='customButton px-4 py-2'> visit sources</button></a>
                  </div>
              
              </div>    
--- src/pages/Homepage/TinyInventory/TinyInventory.js
+++ src/pages/Homepage/TinyInventory/TinyInventory.js
@@ -3,29 +3,41 @@ import "./TinyInventory.css"
 
 const TinyInventory = () => {
     const [products, setproducts] = useState([]);
-    useEffect(() =>{
+    useEffect(() => {
         fetch('fakeData.json')
-        .then(res => res.json())
-        .then(data => setproducts(data));
-    },[])
+            .then(res => res.json())
+            .then(data => setproducts(data));
+    }, [])
     return (
         <div className='tinyInventory '>
             <div className='gradientDivFromBlack'> </div>
-            <h1 className='mt-3 text-center'>Visualization <span className='customRed'>Of</span> Intentory</h1>
+            <h1 className='mt-3 mb-5 text-center'>Visualization <span className='customRed'>Of</span> Intentory</h1>
             <table className='tinyIntevtoryContainer pt-3'>
-                {products.map(product => 
-                <tr className=''>
-                   <td> <img src={product.image} alt="product" /></td>
-                   <td className='customBorder'>{product.name}</td>
-                   <td className='customBorder'>{product.description}</td>
-                   <td className='customBorder'>{product.price}</td>
-                   <td className='customBorder'>{product.supplierName}</td>
-                   <td className='customBorder'>{product.quantity}</td>
-                   <td> <button> Update</button></td>
-                </tr> )
+                <tr>
+                    <th></th>
+                    <th>name</th>
+                    <th>specification</th>
+                    <th>price</th>
+                    <th>quantity</th>
+                    <th>amount</th>
+                    <th></th>
+                </tr>
+                {products.map(product =>
+                    <tr className='datas'>
+                        <td className='ps-2'> <img src={product.image} alt="product" /></td>
+                        <td className=''>{product.name}</td>
+                        <td className=''>{product.description}</td>
+                        <td className=''>{product.price}</td>
+                        <td className=''>{product.supplierName}</td>
+                        <td className=''>{product.quantity}</td>
+                        <td className='pe-2'> <button className='customButton '> Update</button></td>
+                    </tr>)
                 }
-                
             </table>
+            <div className='text-center'>
+                <button className='manageInventoryBtn p-3 mt-3 text-center'><strong> Manage Inventory</strong></button>
+            </div>
+            <div className='gradientDivFromWhite'></div>
         </div>
     );
 };
