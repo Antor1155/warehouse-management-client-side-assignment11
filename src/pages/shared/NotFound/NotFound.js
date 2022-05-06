890a35dee1372df59e62852f1fcf305764386028
Md Antor <md.antor1155@gmail.com>
2022-05-06 15:06:25 +0800

    not found page done

--- src/pages/shared/NotFound/NotFound.js
+++ src/pages/shared/NotFound/NotFound.js
@@ -1,9 +1,21 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import "./NotFound.css"
+import notfound from "../../../Images/notfound/notfound.jpg"
+import { Link } from 'react-router-dom';
 
 const NotFound = () => {
+    useEffect(() => {
+        document.querySelector(".footer").style.display = "none";
+        document.querySelector(".myNav").style.display = "none";
+    }, [])
+
+
     return (
-        <div>
-            <h1>nothing found here</h1>
+        <div className='nothignFound'>
+            <Link to='/home' >
+                <h3><span className='me-3 customRed'>F</span>oo<span className='ms-3 customRed'>D</span></h3>
+            </Link>
+            <img className='errorImg' src={notfound} alt="nothing found" />
         </div>
     );
 };
