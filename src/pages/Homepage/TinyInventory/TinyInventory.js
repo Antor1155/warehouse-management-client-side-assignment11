074485dbc24bf3ad0291c4961ac4814545a6b3fd
Md Antor <md.antor1155@gmail.com>
2022-05-06 18:34:33 +0800

    most fo the error taggled

--- src/pages/Homepage/TinyInventory/TinyInventory.js
+++ src/pages/Homepage/TinyInventory/TinyInventory.js
@@ -8,28 +8,30 @@ const TinyInventory = (props) => {
         <div className='tinyInventory '>
             <div className='gradientDivFromBlack'> </div>
             <h1 className='sectionStart mb-4 text-center'>Visualization <span className='customRed'>Of</span> Intentory</h1>
-            {children? children: ""}
+            {children ? children : ""}
             <table className='tinyIntevtoryTable mt-5'>
-                <tr>
-                    <th> </th>
-                    <th>Name</th>
-                    <th>Description</th>
-                    <th>Price</th>
-                    <th>Supplier</th>
-                    <th>Amount</th>
-                    <th> </th>
-                </tr>
-                {products.map(product =>
-                    <tr className=''>
-                        <td> <img src={product.image} alt="product" /></td>
-                        <td className='customRed'>{product.name}</td>
-                        <td>{product.description}</td>
-                        <td>$ {product.price}</td>
-                        <td>{product.supplierName}</td>
-                        <td>{product.quantity}</td>
-                        <td> <button className='customButton'> Update</button></td>
-                    </tr>)
-                }
+                <tbody>
+                    <tr>
+                        <th> </th>
+                        <th>Name</th>
+                        <th>Description</th>
+                        <th>Price</th>
+                        <th>Supplier</th>
+                        <th>Amount</th>
+                        <th> </th>
+                    </tr>
+                    {products.map(product =>
+                        <tr key={product.id}>
+                            <td> <img src={product.image} alt="product" /></td>
+                            <td className='customRed'>{product.name}</td>
+                            <td>{product.description}</td>
+                            <td>$ {product.price}</td>
+                            <td>{product.supplierName}</td>
+                            <td>{product.quantity}</td>
+                            <td> <button className='customButton'> Update</button></td>
+                        </tr>)
+                    }
+                </tbody>
             </table>
             <div className='text-center '>
                 <button className='manageInventoryBtn mt-4  py-2  px-2 mx-auto'><strong> Manage Inventory</strong></button>
--- src/pages/ManageInventory/ManageInventory.js
+++ src/pages/ManageInventory/ManageInventory.js
@@ -1,15 +1,12 @@
 import React from 'react';
-import useData from '../../customHooks/fetchingAllData/useData';
-import TinyInventory from '../Homepage/TinyInventory/TinyInventory';
 import "./ManageInventory.css"
 
 const ManageInventory = ({setNotFoundPage}) => {
     setNotFoundPage(false);
-    const [products, setProducts]=useData()
     return (
         <div className='manageInvetory'>
             
-            <TinyInventory products={products}></TinyInventory>
+            
         </div>
     );
 };
