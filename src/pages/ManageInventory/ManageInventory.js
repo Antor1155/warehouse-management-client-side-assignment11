12d33505009ed7a8731471eba3f0fb7c1ef8c1a4
Md Antor <md.antor1155@gmail.com>
2022-05-08 18:01:04 +0800

    manage inventory section connected with server side and now delete data form database after confirmation on ui, also update ui

--- src/pages/ManageInventory/ManageInventory.js
+++ src/pages/ManageInventory/ManageInventory.js
@@ -6,12 +6,34 @@ const ManageInventory = ({ setNotFoundPage }) => {
     setNotFoundPage(false);
 
     const [allProducts, setAllProducts] = useState([]);
+    const [deleted, setDeleted] = useState({})
+
+    // getting all product to manage 
     useEffect(() => {
-        fetch("fakeData.json")
-            .then(res =>res.json())
+        fetch("http://localhost:5000/manageAll")
+            .then(res => res.json())
             .then(data => setAllProducts(data))
         document.body.style = 'background:rgb(240, 238, 238)';
-    }, [])
+    }, [, deleted])
+
+
+    // deleting item form database and ui 
+    const handleDelete = id => {
+
+        const confirm = window.confirm("really want to delete", id);
+        if (confirm) {
+            fetch(`http://localhost:5000/deleteItem/${id}`,{
+                method:'delete'
+            })
+                .then(res => res.json())
+                .then(data => setDeleted(data));
+        }
+
+
+
+
+    }
+
     return (
         <div className='manageInvetory text-center'>
             <h2 className=' mt-5'> Manage <span className='customRed'>all {allProducts?.length}</span> products</h2>
@@ -27,14 +49,14 @@ const ManageInventory = ({ setNotFoundPage }) => {
                         <th> </th>
                     </tr>
                     {allProducts.map(product =>
-                        <tr key={product.id}>
+                        <tr key={product._id}>
                             <td> <img src={product.image} alt="product" /></td>
                             <td className='customRed'>{product.name}</td>
                             <td>{product.description}</td>
                             <td>$ {product.price}</td>
                             <td>{product.supplierName}</td>
                             <td>{product.quantity}</td>
-                            <td> <button className='customButton px-3'> Delete</button></td>
+                            <td> <button onClick={() => handleDelete(product._id)} className='customButton px-3'> Delete</button></td>
                         </tr>)
                     }
                 </tbody>
