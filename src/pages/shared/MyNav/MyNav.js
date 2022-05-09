ecdce95b5d804b5b4648990333d0bb7fc99ed962
Md Antor <md.antor1155@gmail.com>
2022-05-09 17:20:06 +0800

    nearly final

--- src/pages/shared/MyNav/MyNav.js
+++ src/pages/shared/MyNav/MyNav.js
@@ -1,5 +1,6 @@
 import { signOut } from 'firebase/auth';
 import React from 'react';
+import { Dropdown } from 'react-bootstrap';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { Link } from 'react-router-dom';
 import auth from '../../../utilities/firebase.init';
@@ -11,25 +12,28 @@ const MyNav = () => {
         signOut(auth);
     }
     return (
-            <div className='myNav mx-auto'>
-                <div>
-                    <Link to='/home' >
-                        <h3 className=''><span className='me-3 customRed'>F</span>oo<span className='ms-3 customRed'>D</span></h3>
-                    </Link>
-                </div>
-
-                <div className='navlinks'>
-                    <Link to="/home">Home</Link>
-                    <Link to="/blogs">Blog</Link>
-                    {user ? <>
-                        <Link to="/manageInventory">Manage Inventory</Link>
-                        <Link to="/additems">Add items</Link>
-                        <Link to="/myitems">My items</Link>
-                    </> : ''}
-                    {user ? <button className='signOutBtn' onClick={HandleSignOut}>SignOut</button> : <Link to="/login">Login</Link>}
-                </div>
-                
+        <div className='myNav mx-auto'>
+            <div>
+                <Link to='/home' >
+                    <h3 className=''><span className='me-3 customRed'>F</span>oo<span className='ms-3 customRed'>D</span></h3>
+                </Link>
             </div>
+
+            <div className='navlinks'>
+                <Link to="/home">Home</Link>
+                <Link to="/blogs">Blog</Link>
+                {user ? <>
+                    <Link to="/manageInventory">Manage Inventory</Link>
+                    <Link to="/additems">Add items</Link>
+                    <Link to="/myitems">My items</Link>
+                </> : ''}
+                {user ? <button className='signOutBtn' onClick={HandleSignOut}>SignOut</button> : <Link to="/login">Login</Link>}
+            </div>
+
+
+           
+
+        </div>
     );
 };
 
