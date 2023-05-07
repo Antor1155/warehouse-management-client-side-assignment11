986988874f5d6ac45a1e1647acd59366e580a724
Md Antor <69227191+Antor1155@users.noreply.github.com>
2023-05-08 03:02:06 +0600

    added measurement id in firebaseConfig file

--- src/utilities/firebase.init.js
+++ src/utilities/firebase.init.js
@@ -9,7 +9,8 @@ const firebaseConfig = {
   projectId: process.env.REACT_APP_projectId,
   storageBucket: process.env.REACT_APP_storageBucket,
   messagingSenderId: process.env.REACT_APP_messagingSenderId,
-  appId: process.env.REACT_APP_appId
+  appId: process.env.REACT_APP_appId,
+  measurementId: process.env.REACT_APP_measurementId
 };
 
 // Initialize Firebase
